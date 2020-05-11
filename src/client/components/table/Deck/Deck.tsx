38764ffefea8bd911f5a89feb97bf6c3dd175ccb
neuroweet@gmail.com <sa3Keeth>
2020-05-11 15:20:24 +0300

    card pick done

--- src/client/components/table/Deck/Deck.tsx
+++ src/client/components/table/Deck/Deck.tsx
@@ -5,12 +5,9 @@ import {cardAspectRatio} from 'shared/constant/cards';
 import GameController from 'client/controllers/gameController';
 import {playerRoomDiag} from 'client/helpers/roomHelpers';
 import {ECardType} from 'shared/enum/cards';
-import {Container, Sprite, Text} from 'react-pixi-fiber';
+import {Container, Text} from 'react-pixi-fiber';
 import Card from 'client/components/table/Card/Card';
 import {getWindowHeight, getWindowWidth} from 'client/helpers/window';
-import {resources} from 'client/resources/resources';
-import * as PIXI from 'pixi.js';
-import {ETurnState} from 'shared/enum/player';
 import {get} from 'lodash';
 import {ENotificationAction} from 'shared/enum/notifications';
 
@@ -23,15 +20,10 @@ const Deck = observer(({controller}: IDeckProps) => {
 	const {playersList, deck} = controller;
 	const width = playerRoomDiag(playersList.length);
 	const topCardType = deck.topCardType;
-	const cardHeight = width * cardAspectRatio;
-	const deckCounterBgTexture = PIXI.Texture.from(resources.deckCounterBg)
-	const counterBadgeSize = width/1.5;
 	const inCardPick = get(controller, ['currentAction', 'type']) === ENotificationAction.cardPick;
+	const fontSize = width/6
 	return (
 		<Container x={getWindowWidth()/2} y={getWindowHeight()/2}>
-			<Sprite texture={deckCounterBgTexture} x={0} y={-cardHeight/2} anchor={0.5} width={counterBadgeSize} height={counterBadgeSize}>
-				<Text text={controller.deck.count+''} y={-counterBadgeSize * 0.8} anchor={0.5} style={{fontFamily : 'Arial', fontSize: counterBadgeSize/1.5, fill : 0xFFFFFF}}/>
-			</Sprite>
 			<Card
 				id={topCardType === ECardType.panic ? 'panicBack' : 'eventBack'}
 				canBeUsed={inCardPick}
@@ -43,6 +35,7 @@ const Deck = observer(({controller}: IDeckProps) => {
 					angle: 0
 				}}
 			/>
+			<Text text={controller.deck.count+''} y={(width * cardAspectRatio / 2) - fontSize} anchor={0.5} style={{fontFamily : 'Arial', fontSize, fill : 0xFFFFFF}}/>
 		</Container>
 	)
 });
