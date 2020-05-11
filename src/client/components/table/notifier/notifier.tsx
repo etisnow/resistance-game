15f2b67e62e41c46ace6040dc6c8c49b26144a8a
neuroweet@gmail.com <sa3Keeth>
2020-05-11 19:37:18 +0300

    notifier text added

--- src/client/components/table/notifier/notifier.tsx
+++ src/client/components/table/notifier/notifier.tsx
@@ -24,28 +24,34 @@ interface INotifierProps {
 	controller:  GameController;
 }
 
-/*
-const generateCardMenuByNotificationType = (controller: GameController, notification: INotificationAction, cardUniqueId?: string) => {
-	return (<div className={'notificationMenuWrapper'}>
-
-		<div
-			className={'notificationMenuItem'}
-			onClick={() => controller.selectCard(notification, cardUniqueId)}
-		>
-			Выбрать
-		</div>
-	</div>)
-};*/
+const getFontStyle = (fontSize, maxWidth) => ({
+    align: "center",
+    dropShadow: true,
+    dropShadowAngle: 90,
+    dropShadowBlur: 4,
+    dropShadowColor: "#63fe6a",
+    dropShadowDistance: 1,
+    fill: "white",
+    letterSpacing: 1,
+    lineHeight: 26,
+    lineJoin: "round",
+    stroke: "#91ff88",
+    textBaseline: "bottom",
+    wordWrap: true,
+    fontSize: fontSize,
+    wordWrapWidth: maxWidth
+})
 
 const Notification = ({notification, controller}: {notification: INotificationAction, controller: GameController}) => {
 	let notificationContent: React.ReactNode = null;
 	const okayTexture = getPixiTexture(resources.okay)
-	const pivotAtCenter = {x:-getWindowWidth() / 2 , y: getWindowHeight()/2}
+	//const pivotAtCenter = {x:-getWindowWidth() / 2 , y: getWindowHeight()/2}
+	const notificationFontSize = 22;
 	switch (notification.type) {
 		case ENotificationAction.okayCard:
 			const cardHeight = autoWidthCard(Object.keys(notification.cards).length) * cardAspectRatio;
 			notificationContent = (
-				<Container width={getWindowWidth()} height={getWindowHeight()}>
+				<React.Fragment>
 					<HandComponent
 						cards={notification.cards}
 						selectedCardIndex={null}
@@ -66,20 +72,48 @@ const Notification = ({notification, controller}: {notification: INotificationAc
 						x={getWindowWidth() / 2}
 						y={getWindowHeight()/2 +  cardHeight / 2}
 					/>
-				</Container>
+					<Text
+						x={getWindowWidth() / 2}
+						y={getWindowHeight()/2 -  cardHeight / 2 - notificationFontSize * 2}
+						text={notification.text}
+						anchor={0.5}
+						style={getFontStyle(18, getWindowWidth() * 0.8)}
+					/>
+				</React.Fragment>
 			);
 			break;
 		case ENotificationAction.selectCard:
 			//const menu = (cardUniqueId) => generateCardMenuByNotificationType(controller, notification, cardUniqueId);
 			notificationContent = (
-				<HandComponent
-					cards={notification.cards}
-					selectedCardIndex={null}
-					autoWidth={true}
-					cardActions={{}}
-					onSelectCard={() => {}}
-					onCardAction={() => {}}
-					y={getWindowHeight()/2 - playerHandHeight() /2}
+				<React.Fragment>
+					<HandComponent
+						cards={notification.cards}
+						selectedCardIndex={null}
+						autoWidth={true}
+						cardActions={{}}
+						onSelectCard={() => {}}
+						onCardAction={() => {}}
+						y={getWindowHeight()/2 - playerHandHeight() /2}
+					/>
+					<Text
+						x={getWindowWidth() / 2}
+						y={getWindowHeight()/2 -  cardHeight / 2 - notificationFontSize * 2}
+						text={notification.text}
+						anchor={0.5}
+						style={getFontStyle(18, getWindowWidth() * 0.8)}
+					/>
+				</React.Fragment>
+			);
+			break;
+		case ENotificationAction.info:
+			//const menu = (cardUniqueId) => generateCardMenuByNotificationType(controller, notification, cardUniqueId);
+			notificationContent = (
+				<Text
+					x={getWindowWidth() / 2}
+					y={getWindowHeight()/2 -  cardHeight / 2 - notificationFontSize * 2}
+					text={notification.text}
+					anchor={0.5}
+					style={getFontStyle(18, getWindowWidth() * 0.8)}
 				/>
 			);
 			break;
@@ -91,8 +125,9 @@ const Notification = ({notification, controller}: {notification: INotificationAc
 			<Container alpha={0.7}>
 				<Rectangle xCoord={0} yCoord={0} width={getWindowWidth()} height={getWindowHeight()}/>
 			</Container>
-			<Text text={notification.text} />
-			{notificationContent}
+			<Container width={getWindowWidth()} height={getWindowHeight()}>
+				{notificationContent}
+			</Container>
 		</Container>
 	)
 };
